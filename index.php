36d28e699776250ea7d011b7eec3fa6cabde4f44
Chamila Jayaratne <cjchamila@gmail.com>
2024-05-06 15:21:49 +0530

    commit project files

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lange="en">
+<head>
+
+<meta charset="UTF-8">
+<title>Document</title>
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<script type="text/javascript">
+ 
+</script>
+</head>
+<body>
+
+
+<h1>Restaurant Reservation System</h1>
+<h2>Please navigate using the following links</h2>
+
+<div class="page-links">
+  <ul>
+    <li><a href="http://my-restaurant.com/pages/restaurant_reservation.php">New Reservation</a></li>
+    <li><a href="http://my-restaurant.com/pages/menu_items.php">Browse Menu Items</a></li>
+    <li><a href="http://my-restaurant.com/pages/menu_items_filter.php">Browse Menu Items by Category</a></li>
+    <li><a href="http://my-restaurant.com/pages/reservation_management.php">Manage Reservations</a></li>
+    <li><a href="http://my-restaurant.com/pages/contact_form.php">Contact us</a></li>
+  </ul>
+</div>
+</body>
+</html>
