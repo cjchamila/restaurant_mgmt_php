0522e2453eba6542650066f9bd7112fc1c9fd9fb
Chamila Jayaratne <cjchamila@gmail.com>
2024-05-07 10:26:51 +0530

    fix index page urls

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
 
 <div class="page-links">
   <ul>
-    <li><a href="http://my-restaurant.com/pages/restaurant_reservation.php">New Reservation</a></li>
-    <li><a href="http://my-restaurant.com/pages/menu_items.php">Browse Menu Items</a></li>
-    <li><a href="http://my-restaurant.com/pages/menu_items_filter.php">Browse Menu Items by Category</a></li>
-    <li><a href="http://my-restaurant.com/pages/reservation_management.php">Manage Reservations</a></li>
-    <li><a href="http://my-restaurant.com/pages/contact_form.php">Contact us</a></li>
+    <li><a href="http://www.my-restaurant.com/pages/restaurant_reservation.php">New Reservation</a></li>
+    <li><a href="http://www.my-restaurant.com/pages/menu_items.php">Browse Menu Items</a></li>
+    <li><a href="http://www.my-restaurant.com/pages/menu_items_filter.php">Browse Menu Items by Category</a></li>
+    <li><a href="http://www.my-restaurant.com/pages/reservation_management.php">Manage Reservations</a></li>
+    <li><a href="http://www.my-restaurant.com/pages/contact_form.php">Contact us</a></li>
   </ul>
 </div>
 </body>
